f104a159cd1ddd94a977f83836fdde3c4c5ccd9d
panjialbir <115150326+panjialbir@users.noreply.github.com>
2023-11-16 12:06:34 +0700

    add ORDERUISTATE

---
+++ app/src/main/java/com/example/praktikum6/data/OrderUIState.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum6.data
+
+data class OrderUIState(
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String = "",
+    val name : String = "",
+    val notlp : String = "",
+    val almt : String = ""
+)
